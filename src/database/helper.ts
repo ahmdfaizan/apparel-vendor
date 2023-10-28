0fd9eda36c4c1a8b954b60659a8c43b7a521a760
Faizan Zargar <faizan.zargar@neosoftmail.com>
2023-10-28 22:52:22 +0530

    Added: Path resolve

--- src/database/helper.ts
+++ src/database/helper.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
+import path from "path";
 import { Apparels } from "../types";
-const dataFile = "src/database/data.json";
+
+let dataFilePath = path.resolve(__dirname, "data.json");
 
 // data structure
 let data: Apparels = {}; //# change to type
@@ -8,10 +10,8 @@ let data: Apparels = {}; //# change to type
 // prepare dataSource from json file
 export function dataSource() {
   try {
-    data = JSON.parse(fs.readFileSync(dataFile, "utf-8"));
-    console.log(data);
+    data = JSON.parse(fs.readFileSync(dataFilePath, "utf-8"));
   } catch (error) {
-    console.log("Error: ", error);
     data = {};
   } finally {
     return data;
@@ -20,6 +20,6 @@ export function dataSource() {
 
 // save data to the JSON file
 export function saveData(toSave: Apparels) {
-  fs.writeFileSync(dataFile, JSON.stringify(toSave, null, 2), "utf-8");
+  fs.writeFileSync(dataFilePath, JSON.stringify(toSave, null, 2), "utf-8");
   data = toSave;
 }
